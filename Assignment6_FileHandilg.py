7c66c0f4bde052e9e61b8e55c8b53b139ee71fd3
aswin <aswinpdas83@gmail.com>
2022-05-31 20:55:11 +0530

    first commit.

--- Assignment6_FileHandilg.py
+++ Assignment6_FileHandilg.py
@@ -1,48 +1,53 @@
 def createFile(file_name):
     try:
-        file1=open(file_name,"w")
+        file1 = open(file_name, "w")
         file1.write("Hai Aswin, Welcome to Avodha")
         file1.close()
-        return "file created with name "+file_name
+        return "file created with name " + file_name
     except:
         return "Something Wrong..."
 
+
 def readFile(file_name):
     try:
-        file1=open(file_name,"r")
-        content=file1.read()
+        file1 = open(file_name, "r")
+        content = file1.read()
         file1.close()
         return content
     except:
         return "File not found"
 
-def addContentToFile(file_name,content):
+
+def addContentToFile(file_name, content):
     try:
-        file1 = open(file_name,"a")
-        file1.write("\n"+content)
+        file1 = open(file_name, "a")
+        file1.write("\n" + content)
         file1.close()
         return "content added to the file"
     except:
         return "Somthing Wrong"
 
+
 def mainFunction():
-    i=str(input("\nTo create file Enter 1 \nTo read file Enter 2\nTo add content Enter 3\nTo exit press any key\n"))
-    if i=="1":
-        file_name=input("Enter the file name : ")
+    i = str(input("\nTo create file Enter 1 \nTo read file Enter 2\nTo add content Enter 3\nTo exit press any key\n"))
+    if i == "1":
+        file_name = input("Enter the file name : ")
         print(createFile(file_name))
         mainFunction()
-    elif i=="2":
-        file_name=input("Enter the file name : ")
+    elif i == "2":
+        file_name = input("Enter the file name : ")
         print(readFile(file_name))
         mainFunction()
-    elif i=="3":
+    elif i == "3":
         file_name = input("Enter the file name : ")
-        content=input("Type Your content : ")
-        print(addContentToFile(file_name,content))
+        content = input("Type Your content : ")
+        print(addContentToFile(file_name, content))
         mainFunction()
     else:
 
         return
+
+
 mainFunction()
 print("Bye...")
 
