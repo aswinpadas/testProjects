3e176d0d7093370bedf8cd0e13294c15d73e3ed9
aswin <aswinpdas83@gmail.com>
2022-06-01 12:43:25 +0530

    no change

--- unittestbullsandcows.py
+++ unittestbullsandcows.py
@@ -3,10 +3,6 @@ import unittest
 from bullsandcows import *
 class MyTestCase(unittest.TestCase):
     def test_scrt_code(self):
-        actual=
-        expected=
         self.assertEqual(True, False)  # add assertion here
-
-
 if __name__ == '__main__':
     unittest.main()
