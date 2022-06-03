563cc37a52ef2dc67f18091fc9cbb7c72f5635fb
aswin <aswinpdas83@gmail.com>
2022-06-03 16:57:04 +0530

    still working in this project. this file is only a staring not completed

---
+++ autoGitCMD.py
@@ -0,0 +1,17 @@
+import os
+import tkinter
+# from git import *
+
+# REPO_DIR = os.path.join(os.getcwd(), 'repository')
+# repo = Repo.init(REPO_DIR)
+# status = repo.git.status()
+# obj_t=tkinter.Tk()
+# # obj_t.title('Auto Git')
+# obj_t.geometry('100x100')
+# def funCommand(command):
+#     os.system(command)
+#
+# btn = tkinter.Button(obj_t, text='Git Status',command=funCommand('\"C:\\Users\\aswin\Desktop\\novel>git status\"')).place()
+# obj_t.mainloop()
+
+os.system('cmd/ \C:\\Users\\aswin\Desktop\\novel "git status"')
